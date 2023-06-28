e27ca6cbbecf131decc536a79125abc1cea81fb8
steevened <alvaradosteven315@gmail.com>
2023-06-28 16:18:27 -0500

    style cards and create item task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
+  // metadata to specify the HTML template and styles that belongs to the component
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tasks';
+  title = 'Tasks App';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems: Item[] = [
+    {
+      description: 'Buy milk',
+      done: false,
+    },
+    {
+      description: 'Save the world',
+      done: false,
+    },
+    {
+      description: 'Feed the cat',
+      done: true,
+    },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+
+    return this.allItems.filter((item) => {
+      return this.filter === 'done' ? item.done : !item.done;
+    });
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css'],
+})
+export class ItemComponent {
+  editable: boolean = false;
+
+  @Input() item!: Item;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description: string) {
+    if (!description) return;
+    this.editable = false;
+    this.item.description = description;
+  }
+}
